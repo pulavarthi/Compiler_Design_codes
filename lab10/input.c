b6f1dbad5631892fc77e9031abe36dcd18ce7083
Sandeep_Pulavarthi <sandeep.pulavarthi17@gmail.com>
2018-03-27 03:47:13 +0530

    Create input.c

---
+++ lab10/input.c
@@ -0,0 +1,20 @@
+main(){
+	int x,y,z[10];
+	char str[20];
+	x=1;
+	y=z+y*z/10;
+	while(x)
+	{
+		for(i=0;i<100;i=i+2)
+		{
+			x=y+1;
+			y=y+2;
+		}
+		for(i=0;i<20;i=i+1)
+		{
+			
+		}
+	}
+}
+
+	
